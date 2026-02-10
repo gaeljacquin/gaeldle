0373c785e52e33cce449e02fc4b72832e2668b7b
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2026-02-10 14:35:05 -0500

    feat: stack auth
    removed pre-install script

--- apps/web2/app/layout.tsx
+++ apps/web2/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackClientApp } from "../stack/client";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
 import "./globals.css";
 
@@ -28,9 +30,9 @@ export default function RootLayout({
     <html lang="en" className={figtree.variable}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><StackProvider app={stackClientApp}><StackTheme>
         {children}
-      </body>
+      </StackTheme></StackProvider></body>
     </html>
   );
 }
